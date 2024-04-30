b28e9f435a28b3b6e7d891dbad66e46e60e47605
Ion Lefter <ionlefter1997@gmail.com>
2024-04-30 10:41:38 +0300

    add proxy server

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,7 +10,7 @@ export class CartService {
 
   constructor() { }
 
-  addToCart(product: IProduct){
+  add(product: IProduct){
     this.cart.push(product);
     console.log(`product ${product.name} added to cart`)
   }
